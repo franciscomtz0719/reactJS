f32b7cfb20576871641a7b34fd2e0cd8cfd9929c
franciscomtz0719 <franciscomtz0719@gmail.com>
2022-07-22 19:26:20 -0500

    add botones react

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import './App.css'
-import List from './componentes/List/'
-import users from './mocks/mock_user.js'
+import React, { useState } from "react";
+import  Button from './Components/Button/index';
 
-function App () {
-  console.log(users)
+function App() {
+  const [colors, setColors] = useState();
+
   return (
-    <div className='App'>
-      <List data={users} />
-    </div>
-  )
-}
+    <>
+      <button
+        className={'green m-4'}
+        onClick={() => setColors((colors) => (colors = "bg-success"))}
+      >
+        Button green
+      </button>
+      <button
+        className='red m-4'
+        onClick={() => setColors((colors) => (colors = "bg-danger"))}
+      >
+        Button Red
+      </button>
+      <button
+        className='yellow m-4'
+        onClick={() => setColors((colors) => (colors = "bg-warning"))}
+      >
+        Button Yellow
+        
+      </button>
 
-export default App
+      <div className= {`circle ${colors} d-flex align-items-center text-center m-4`} >Semaforo</div>
+
+    </>
+  )}
+
+  export default App
